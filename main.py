0964d9d2c655f86bcee560f9eb0822307259b71b
Max Margalith <max.margalith@dremio.com>
2024-06-15 21:03:51 +0200

    added SQL code generation and additional validation

--- main.py
+++ main.py
@@ -34,7 +34,38 @@ def validate_config(config: dict):
             raise ValueError(f"Folder path exclusion {p} does not match a subtree of the scope path {scope_path}")
         if len(p) == len(scope_path):
             raise ValueError(f"Folder path exclusion {p} cannot be the same depth as the scope path {scope_path}")
+        
+    # Validate if roles exist in AD?
+    # TBD
 
+    # Validate folder privileges (Source: https://docs.dremio.com/current/security/rbac/privileges/#folder-privileges)
+    DREMIO_FOLDER_PRIVILEGES = {
+        "ALTER",
+        "ALTER REFLECTION",
+        "MANAGE GRANTS",
+        "MODIFY",
+        "OWNERSHIP",
+        "READ METADATA",
+        "SELECT",
+        "VIEW REFLECTION",
+    }
+    for p in config["GRANT PRIVILEGES"]:
+        if p not in DREMIO_FOLDER_PRIVILEGES:
+            raise ValueError(f'Unexpected privilege "{p}" for type folder, please see the Dremio docs for supported privileges: https://docs.dremio.com/current/security/rbac/privileges/#folder-privileges')
+
+
+def generate_grant_sql(config: dict, catalog_entries: list[dict]):
+    privileges: list[str] = config["GRANT PRIVILEGES"]
+    grantee_roles: list[str] = config["TO ROLES"]
+    sql_statements = []
+
+    for privilege in privileges:
+        for c in catalog_entries:
+            folder = '"."'.join(c["folder_path"])
+            for role in grantee_roles:
+                sql_statements.append(f'GRANT {privilege} ON FOLDER "{folder}" TO ROLE "{role}";')
+    
+    return sql_statements
 
 
 if __name__ == '__main__':
@@ -49,8 +80,7 @@ if __name__ == '__main__':
             "VIEW REFLECTION"
         ],
         "ON SCOPE PATH": [
-            "demo_space",
-            "subfolder"
+            "demo_space"
         ],
         "EXCLUDING FOLDER PATHS": [
             [
@@ -60,20 +90,34 @@ if __name__ == '__main__':
             ],
             [
                 "demo_space",
-                "subfolder",
-                "AppFolder2"
+                "subfolder2",
+                "subsubfolder2_2",
+                "app2"
             ],
         ],
         "TO ROLES": [
-            "User1"
+            "Role1",
+            "Role2"
         ]
     }
 
     api = dremio_api.DremioAPI(DREMIO_PAT, DREMIO_ENDPOINT, timeout=60)
     validate_config(config)
     catalog_entries = traverse_dremio_catalog(api, config)
+    sql_statements = generate_grant_sql(config, catalog_entries)
 
     # Results
-    print("The following folders are in the scope:")
-    for c in catalog_entries:
-        print(c)
+    print("\n__________")
+    print("Pseudo-SQL:")
+    print(f"""
+          GRANT {config["GRANT PRIVILEGES"]} 
+          ON SPACE/FOLDER {config["ON SCOPE PATH"]} 
+          EXLCUDING FOLDER(s) {config["EXCLUDING FOLDER PATHS"]} 
+          TO ROLE(s) {config["TO ROLES"]};
+          """)
+    print("The following SQL statements need to be run in order to provide the same scope as the pseudo-SQL above:")
+    print("\n__________")
+    for sql in sql_statements:
+        print(sql)
+    print("__________\n")
+
